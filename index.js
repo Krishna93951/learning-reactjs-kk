3a4f2a77d5fe7ceaf06d3ad7bba1a6f589c94e97
Krishna93951 <kkagile123@gmail.com>
2019-12-20 15:21:47 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,43 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-class App extends React.Component {
-  constructor(prop) {
-    this.state = prop.time;
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
   }
 
-  countDown() {
-    this.setState(state => ({
-      count: state.count > 0 ? state.count - 1 : state.count
-    }));
-  }
-
-  countUp() {
-    this.setState(state => ({
-      count1: state.count1 < 10 ? state.count1 + 1 : state.count1
-    }));
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.countDown();
-      this.countUp();
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Count Down :{this.state.count}</h1>
-        <h2>Increment :{this.state.count1}</h2>
-      </div>
-    );
-  }
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
 }
 
-const timer = {
-  count: 10,
-  count1: 0
-};
-
-ReactDOM.render(<App time={timer} />, document.getElementById("root"));
+ReactDOM.render()
