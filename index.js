230e8f1e803a9247b89164f301ff4dbb2ec876ef
Krishna93951 <kkagile123@gmail.com>
2019-12-20 15:27:43 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-function ActionLink() {
+const action = function ActionLink() {
   function handleClick(e) {
     e.preventDefault();
     console.log('The link was clicked.');
   }
 
   return (
-    <a href="#" onClick={handleClick}>
+    <button  onClick={handleClick}>
       Click me
-    </a>
+    </button>
   );
 }
 
-ReactDOM.render()
+ReactDOM.render(action,document.getElementById('root'));
